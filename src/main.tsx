2e8faa1f18f8566eff531c3ff31db5aeb4c5649b
davidKH <davidjb06.kodehode@gmail.com>
2023-11-21 10:05:27 +0100

    fixed my routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./css/index.css";
-
-import Home from "./routes/Home";
-import Links from "./routes/Links/LinkPage";
-import About from "./routes/About/AboutPage";
-import Error from "./routes/Error/Error";
-import Layout from "./routes/Layout";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout></Layout>,
-    errorElement: <Error></Error>,
-    children: [
-      {
-        index: true,
-        element: <Home></Home>,
-      },
-      {
-        path: "about/",
-        element: <About></About>,
-      },
-      {
-        path: "Links/",
-        element: <Links></Links>,
-      },
-    ],
-  },
-  {
-    path: "Error/",
-    element: <Error></Error>,
-  },
-]);
+import App from "./routes/app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
---
+++ src/routes/app.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About/AboutPage";
+import Error from "./Error/Error";
+import Links from "./Links/LinkPage";
+import Layout from "./Layout";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="links" element={<Links />} />
+
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+export default App;
